743d19d380a70fb443b251b20ba859cf8038c1ea
Vladislav Tikhomirov <vladislav.tihomirov@studiomobile.ru>
2018-11-13 19:13:23 +0300

    <master> init commit

---
+++ WebApp/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    // TODO 5: unauthorized users should receive 401 status code and should be redirected to Login endpoint   
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _service;
+        
+        public AccountController(IAccountService service)
+        {
+            _service = service;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ValueTask<Account> Get()
+        {
+            return _service.LoadOrCreateAsync(null /* TODO 3: Get user id from cookie */);
+        }
+        
+        //TODO 6: Get user id from cookie
+        //TODO 7: Endpoint should works only for users with "Admin" Role
+        [Authorize]
+        [HttpGet]
+        public Account GetByInternalId([FromQuery] int internalId)
+        {
+            return _service.GetFromCache(internalId);
+        }
+
+        [Authorize]
+        [HttpPost("counter")]
+        public async Task UpdateAccount()
+        {
+            //Update account in cache, don't bother saving to DB, this is not an objective of this task.
+            var account = await Get();
+            account.Counter++;            
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# School Test App
+
+**The point of exercise** is solve as many TODOs as you can. Please Feel free to send your solution if you completed at least 4 TODOs. Every task can have a lot of different solutions, think about your code style, performance, about balance between code style and performance :), and about possible bugs.
+
+
+
+Now clone this repository and get on with the task. Good luck! ;)
