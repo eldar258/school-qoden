6eb6cec0747100a16a5ab57be69741a5932487dc
eldar258 <9eldik7@gmail.com>
2018-12-02 08:36:49 +0300

    TODO1,2. Аутентифицировать пользователя
    Аутентификация пользователя по имени (Email). Если нет пользователя в базе, вывод "404".

--- WebApp/LoginController.cs
+++ WebApp/LoginController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using System.Collections.Generic;
 
 namespace WebApp
 {
@@ -14,14 +18,24 @@ namespace WebApp
         }
         
         [HttpPost("sign-in")]
-        public async Task Login(string userName)
+        public async Task<IActionResult> Login(string userName)
         {
             var account = await _db.FindByUserNameAsync(userName);
-            if (account != null)
-            {
-                //TODO 1: Generate auth cookie for user 'userName' with external id
+            if (account != null) {
+                //*TODO 1: Generate auth cookie for user 'userName' with external id
+                await authenticate(account.ExternalId);
+                return Ok();
+            } else {
+                //*TODO 2: return 404 not found if user not found
+                return NotFound();
             }
-            //TODO 2: return 404 not found if user not found
+        }
+
+        private async Task authenticate(string name) {
+            var defaultClaim = ClaimsIdentity.DefaultNameClaimType;
+            var claims = new List<Claim> { new Claim(defaultClaim, name) };
+            ClaimsIdentity ci = new ClaimsIdentity(claims, "ApplicationCookie", defaultClaim, defaultClaim);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ci));
         }
     }
 }
