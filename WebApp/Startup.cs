11ee4eeb7c1bd4fbbb61fab85050fa72b18e40e4
eldar258 <9eldik7@gmail.com>
2018-12-02 11:42:16 +0300

    TODO5. Редирект для неавторизованных пользователей
    В браузере веб клиента вызврат ошибки "302" и редирект на страницу авторизации. Но при json запросах вохврат ошибки "401" и ссылки на страницу авторизации.

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -16,12 +16,16 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddSingleton<IAccountDatabase, AccountDatabaseStub>();
             services.AddSingleton<IAccountCache, AccountCache>();
             services.AddSingleton<IAccountService, AccountService>();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie( options => {
+                options.LoginPath = new PathString("/api/sign-in");
+            });
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
